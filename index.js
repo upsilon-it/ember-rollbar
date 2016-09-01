2072fb874f51e5b7758c8268609da7fd7f89c947
Dzianis <dzianis.cishuk@upsilonit.com>
2016-09-01 16:56:31 +0300

    Blueprint (#3)
    * Move app.import rollbar to ember-cli-build.
    
    * Add blueprint for rollbar bower component.

--- index.js
+++ index.js
@@ -2,11 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-rollbar',
-
-  included(app) {
-    this._super.included(app);
-
-    app.import(`${ app.bowerDirectory }/rollbar/dist/rollbar.js`);
-  }
+  name: 'ember-rollbar'
 };
