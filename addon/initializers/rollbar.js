52419e8c254b676703a9a4b9b728387176f1c7e2
Alex Ulianytskyi <a.ulyanitsky@gmail.com>
2017-03-30 16:56:43 +0300

    Fix code style for addon/initializers/rollbar

--- addon/initializers/rollbar.js
+++ addon/initializers/rollbar.js
@@ -1,47 +1,54 @@
 /* global Rollbar */
 import Ember from 'ember';
 
+const {
+  Logger,
+  typeOf
+} = Ember;
+
+const stringify = function (object) {
+  return JSON ? JSON.stringify(object) : object.toString();
+};
+
+const isError = function (object) {
+  return typeOf(object) === 'error';
+};
+
 export default {
   name: 'rollbar',
-  initialize: function() {
-    var errorLogger = Ember.Logger.error;
-    Ember.Logger.error = function() {
-      var args = Array.prototype.slice.call(arguments),
-          err = isError(args[0]) ? args[0] : new Error(stringify(args));
 
-      if (window.Rollbar) {
+  initialize: function () {
+    var errorLogger = Logger.error;
+    Logger.error = function (...args) {
+      let err = isError(args[0]) ? args[0] : new Error(stringify(args));
+      if (Rollbar) {
         Rollbar.error.call(Rollbar, err);
       }
       errorLogger.apply(this, arguments);
     };
-    var warnLogger = Ember.Logger.warn;
-    Ember.Logger.warn = function() {
-      if (window.Rollbar) {
+
+    var warnLogger = Logger.warn;
+    Logger.warn = function () {
+      if (Rollbar) {
         Rollbar.warning.apply(Rollbar, arguments);
       }
       warnLogger.apply(this, arguments);
     };
-    var infoLogger = Ember.Logger.info;
-    Ember.Logger.info = function() {
-      if (window.Rollbar) {
+
+    var infoLogger = Logger.info;
+    Logger.info = function () {
+      if (Rollbar) {
         Rollbar.info.apply(Rollbar, arguments);
       }
       infoLogger.apply(this, arguments);
     };
-    var debugLogger = Ember.Logger.debug;
-    Ember.Logger.debug = function() {
-      if (window.Rollbar) {
+
+    var debugLogger = Logger.debug;
+    Logger.debug = function () {
+      if (Rollbar) {
         Rollbar.debug.apply(Rollbar, arguments);
       }
       debugLogger.apply(this, arguments);
     };
   }
-};
-
-var stringify = function(object){
-  return JSON ? JSON.stringify(object) : object.toString();
-};
-
-var isError = function(object){
-  return Ember.typeOf(object) === 'error';
 };
